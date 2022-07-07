2e39a19330002a4b80a363fe3ce7b99dfa58bfa9
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-07 12:01:04 +0300

    added asssets + icon font

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import 'reset-css';
-import './scss/index.scss';
+import './styles/icon-font.css';
+import './styles/index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
