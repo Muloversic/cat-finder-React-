76311e75a56c1310913d26d393aa9a7091640255
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-09 14:40:42 +0300

    created markup for 5 photos

--- src/components/AppContent/BreedsPage/index.js
+++ src/components/AppContent/BreedsPage/index.js
@@ -1,11 +1,13 @@
 import Navbar from '../Navbar';
 import ActionBar from '../ActionBar';
+import GridPhotos from '../GridPhotos';
 import './index.scss';
 const BreedsPage = ({ contentType }) => {
   return (
     <section className="breeds-page content">
       <Navbar />
       <ActionBar contentType={contentType} />
+      <GridPhotos limit={5} />
     </section>
   );
 };
---
+++ src/components/AppContent/GridPhotos/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+const GridPhotos = () => {
+  return (
+    <div className="photos">
+      <div className="photos-item"></div>
+      <div className="photos-item"></div>
+      <div className="photos-item"></div>
+      <div className="photos-item"></div>
+      <div className="photos-item"></div>
+    </div>
+  );
+};
+
+export default GridPhotos;
