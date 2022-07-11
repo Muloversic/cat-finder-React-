d1ae3d8814e6b58744ebdcd8d9234da78c9ca6b0
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-11 16:24:03 +0300

    replaced Navbar component

--- src/components/AppContent/Breeds/SharedBreedsPage/index.jsx
+++ src/components/AppContent/Breeds/SharedBreedsPage/index.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router';
-import Navbar from '../Navbar';
+import Navbar from '../../Navbar';
 const SharedBreedsPage = () => {
   return (
     <section className="breeds-page content">
