6ff4809ec065e1275583bc9442129dc07a4f850d
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-12 13:53:10 +0300

    added possibitly to vote

--- src/components/AppContent/Voting/VotingPage/index.jsx
+++ src/components/AppContent/Voting/VotingPage/index.jsx
@@ -1,21 +1,52 @@
 import Navbar from '../../Navbar';
 import ActionBar from '../ActionBar';
 import UserLog from '../UserLog';
-import { getRandomImage } from '../../../GetAPI';
+import { getRandomImage, sendVotedImage } from '../../../GetAPI';
 import './index.scss';
 import { useEffect, useState } from 'react';
 const VotingPage = ({ currentPageName }) => {
   const [randomImage, setRandomImage] = useState(null);
+  const [subId, setSubId] = useState('Muloversic');
+  const [voteType, setVoteType] = useState('');
+  const [voteBodyRequest, setVoteBodyRequest] = useState({
+    image_id: '',
+    sub_id: '',
+  });
+
   useEffect(() => {
     (async () => {
       const randomImage = await getRandomImage();
       setRandomImage(randomImage);
     })();
 
+    if (voteBodyRequest.image_id) (async () => await sendVotedImage(voteBodyRequest, voteType))();
+
     return () => {};
-  }, []);
+  }, [voteBodyRequest]);
 
   const imageElement = () => (randomImage ? <img src={randomImage[0].url} alt="cat" className="voting-img" /> : null);
+
+  const handleVote = (event) => {
+    const [image] = randomImage;
+    const like = [...event.target.classList].includes('like');
+    const dislike = [...event.target.classList].includes('dislike');
+    const favour = [...event.target.classList].includes('favour');
+
+    if (like) {
+      setVoteType('votes');
+      setVoteBodyRequest({ image_id: image.id, sub_id: subId, value: 1 });
+    }
+
+    if (dislike) {
+      setVoteType('votes');
+      setVoteBodyRequest({ image_id: image.id, sub_id: subId, value: 0 });
+    }
+
+    if (favour) {
+      setVoteType('favourites ');
+      setVoteBodyRequest({ image_id: image.id, sub_id: subId });
+    }
+  };
 
   return (
     <section className="breeds-page content">
@@ -24,15 +55,15 @@ const VotingPage = ({ currentPageName }) => {
       <section className="voting-section">
         <div className="voting-image-container">
           {imageElement()}
-          <div className="voting-actions">
-            <div className="action action-like">
-              <span className="action-icon icon-smile"></span>
+          <div className="voting-actions" onClick={handleVote}>
+            <div className="action action-like like">
+              <span className="action-icon icon-smile like"></span>
             </div>
-            <div className="action action-favour">
-              <span className="action-icon icon-heart"></span>
+            <div className="action action-favour favour">
+              <span className="action-icon icon-heart favour"></span>
             </div>
-            <div className="action action-dislike">
-              <span className="action-icon icon-sad-face"></span>
+            <div className="action action-dislike dislike">
+              <span className="action-icon icon-sad-face dislike"></span>
             </div>
           </div>
         </div>
--- src/components/GetAPI/index.js
+++ src/components/GetAPI/index.js
@@ -25,3 +25,22 @@ export const getRandomImage = async () => {
     console.error(e);
   }
 };
+
+export const sendVotedImage = async (body, voteType) => {
+  try {
+    const votedImage = await fetch(`https://api.thecatapi.com/v1/${voteType}`, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': 'bf513bac-3dbf-4809-b46a-6a3c40e1e4ad',
+      },
+    });
+
+    const json = await votedImage.json();
+    console.log('Успех:', JSON.stringify(json));
+    return json;
+  } catch (e) {
+    console.error(e);
+  }
+};
