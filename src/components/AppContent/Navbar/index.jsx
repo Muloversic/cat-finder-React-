454c3ffcd370115e161217a85e8a1501684a3826
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-15 17:28:19 +0300

    feat: added handlers at navbar component

--- src/components/AppContent/Navbar/index.jsx
+++ src/components/AppContent/Navbar/index.jsx
@@ -1,12 +1,25 @@
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 import './index.scss';
-const Navbar = () => {
+const Navbar = ({ setSearchRes }) => {
+  const [inputData, setInputData] = useState('');
+  const navigate = useNavigate();
+  const handleSearch = (e) => {
+    setInputData(e.target.value);
+  };
+
+  const sendInputData = (e) => {
+    e.preventDefault();
+    setSearchRes(inputData);
+    navigate('/search');
+  };
+
   return (
     <nav className="navbar">
       <form className="navbar-form">
-        <input type="text" className="navbar-form-input" placeholder="Search for breeds by name" />
-        <button className="navbar-form-btn icon-search" type="submit"></button>
+        <input type="text" className="navbar-form-input" placeholder="Search for breeds by name" onChange={handleSearch} />
+        <button className="navbar-form-btn icon-search" type="submit" onClick={sendInputData}></button>
       </form>
       <ul className="navbar-list">
         <li className="navbar-list-item">
