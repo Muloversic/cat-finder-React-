1dbde55e0a736026704fc1ef4ba3bd01acedb7c7
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-18 14:26:41 +0300

    feat: user can see dropepd image

--- src/components/AppContent/Modal/index.jsx
+++ src/components/AppContent/Modal/index.jsx
@@ -1,9 +1,48 @@
+import { useState } from 'react';
 import uploadImg from '../../../assets/upload.png';
 import './index.scss';
 const Modal = () => {
+  const [drag, setDrag] = useState(false);
+  const [fileName, setFileName] = useState('');
   const closeModal = () => {
     document.body.classList.remove('lock');
     document.querySelector('.modal').classList.remove('modal-active');
+  };
+
+  const [userImage, setUserImage] = useState('');
+  const dragStartHandler = (e) => {
+    e.preventDefault();
+    setDrag(true);
+  };
+
+  const dragLeaveHandler = (e) => {
+    e.preventDefault();
+    setDrag(false);
+  };
+
+  const onDropHandler = (event) => {
+    event.preventDefault();
+    const text = event.dataTransfer.getData('text');
+    if (text) {
+      const image = <img src={text} className="modal-download-img" />;
+      setUserImage(image);
+    }
+
+    if (!text) {
+      const files = event.dataTransfer.files;
+      setFileName(files[0].name);
+      [].map.call(files, (file) => {
+        if (file.type.match(/^image/)) {
+          let reader = new FileReader();
+          reader.onload = (file) => {
+            const image = <img src={file.target.result} className="modal-download-img" />;
+            setUserImage(image);
+          };
+
+          reader.readAsDataURL(file);
+        }
+      });
+    }
   };
 
   return (
@@ -15,14 +54,22 @@ const Modal = () => {
           Any uploads must comply with the <span>upload guidelines</span> or face deletion.
         </p>
       </div>
-      <div className="modal-drop">
-        <p className="modal-drop-info">
-          <span>Drag here</span> your file or <span>Click here</span> to upload
-        </p>
+      <div
+        className="modal-drop"
+        onDragStart={(e) => dragStartHandler(e)}
+        onDragLeave={(e) => dragLeaveHandler(e)}
+        onDragOver={(e) => dragStartHandler(e)}
+        onDrop={(e) => onDropHandler(e)}
+      >
+        {userImage || (
+          <p className="modal-drop-info">
+            <span>Drag here</span> your file or <span>Click here</span> to upload
+          </p>
+        )}
         <img src={uploadImg} alt="upload-here" className="modal-drop-bg" />
       </div>
-      <p className="modal-text">No file selected</p>
-      <button className="modal-btn-upload">UPLOAD PHOTO</button>
+      <p className="modal-text">{fileName || 'No file selected'} </p>
+      {userImage ? <button className="modal-btn-upload">UPLOAD PHOTO</button> : null}
       <p className="modal-text modal-text--small">Thanks for the Upload - Cat found!</p>
     </div>
   );
