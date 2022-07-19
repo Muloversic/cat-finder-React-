91a0fcab9a1bab77f8d6ecba82c32130c1b98ee9
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-19 20:00:51 +0300

    feat: added burger menu + responsive to 768px

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,29 +1,64 @@
 import SidebarCard from '../SidebarCard';
 import logo from '../../assets/logo.svg';
-
 import cardImageVoting from '../../assets/vote-table.png';
 import cardImageBreeds from '../../assets/pet-breeds.png';
 import cardImageGallery from '../../assets/images-search.png';
-
+import { NavLink } from 'react-router-dom';
 import './index.scss';
 
 const Sidebar = ({ currentPageName }) => {
+  const openMenu = () => {
+    const burger = document.querySelector('.aside-nav-burger');
+    burger.classList.toggle('active');
+  };
+  
   return (
-    <aside className="aside">
-      <img src={logo} alt="logo" className="aside-logo" />
-      <div className="aside-welcome">
-        <h1 className="aside-title">Hi intern!</h1>
-        <p className="aside-text">Welcome to MI 2022 Front-end test</p>
-      </div>
-      <div className="aside-action">
-        <h2 className="aside-action-title">Lets start using The Cat API</h2>
-        <div className="aside-cards card">
-          <SidebarCard cardImage={cardImageVoting} cardType={'voting'} currentPageName={currentPageName} />
-          <SidebarCard cardImage={cardImageBreeds} cardType={'breeds'} currentPageName={currentPageName} />
-          <SidebarCard cardImage={cardImageGallery} cardType={'gallery'} currentPageName={currentPageName} />
+    <>
+      <nav className="aside-nav">
+        <div className="aside-nav-burger" onClick={openMenu}>
+          <span className="aside-nav-burger-line"></span>
+          <span className="aside-nav-burger-line"></span>
+          <span className="aside-nav-burger-line"></span>
         </div>
-      </div>
-    </aside>
+        <div className="aside-nav-body">
+          <img src={logo} alt="logo" className="aside-logo" />
+          <h1 className="aside-title">Hi intern!</h1>
+          <p className="aside-text">Welcome to MI 2022 Front-end test</p>
+          <ul className="aside-nav aside-nav-list">
+            <li className="aside-nav-item">
+              <NavLink to="/voting" className="aside-nav-link">
+                Voting
+              </NavLink>
+            </li>
+            <li className="aside-nav-item">
+              <NavLink to="/breeds" className="aside-nav-link">
+                Breeds
+              </NavLink>
+            </li>
+            <li className="aside-nav-item">
+              <NavLink to="/gallery" className="aside-nav-link">
+                Gallery
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <aside className="aside">
+        <img src={logo} alt="logo" className="aside-logo" />
+        <div className="aside-welcome">
+          <h1 className="aside-title">Hi intern!</h1>
+          <p className="aside-text">Welcome to MI 2022 Front-end test</p>
+        </div>
+        <div className="aside-action">
+          <h2 className="aside-action-title">Lets start using The Cat API</h2>
+          <div className="aside-cards card">
+            <SidebarCard cardImage={cardImageVoting} cardType={'voting'} currentPageName={currentPageName} />
+            <SidebarCard cardImage={cardImageBreeds} cardType={'breeds'} currentPageName={currentPageName} />
+            <SidebarCard cardImage={cardImageGallery} cardType={'gallery'} currentPageName={currentPageName} />
+          </div>
+        </div>
+      </aside>
+    </>
   );
 };
 
