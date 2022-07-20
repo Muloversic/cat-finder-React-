6c8653e30439c3a23746a5a80c513129ae8eda97
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-20 13:33:53 +0300

    fix: fixed styles for voting and log sections

--- src/components/AppContent/Voting/VotingPage/index.jsx
+++ src/components/AppContent/Voting/VotingPage/index.jsx
@@ -96,24 +96,26 @@ const VotingPage = ({ currentPageName, subId }) => {
   return (
     <section className="breeds-page content">
       <Navbar />
-      <ActionBar currentPageName={currentPageName} />
-      <section className="voting-section">
-        <div className="voting-image-container">
-          {imageElement()}
-          <div className="voting-actions" onClick={handleVote}>
-            <div className="action action-like like">
-              <span className="action-icon icon-smile like"></span>
-            </div>
-            <div className="action action-favour favour">
-              <span className="action-icon icon-heart favour"></span>
-            </div>
-            <div className="action action-dislike dislike">
-              <span className="action-icon icon-sad-face dislike"></span>
+      <div className="content-wrapper">
+        <ActionBar currentPageName={currentPageName} />
+        <section className="voting-section">
+          <div className="voting-image-container">
+            {imageElement()}
+            <div className="voting-actions" onClick={handleVote}>
+              <div className="action action-like like">
+                <span className="action-icon icon-smile like"></span>
+              </div>
+              <div className="action action-favour favour">
+                <span className="action-icon icon-heart favour"></span>
+              </div>
+              <div className="action action-dislike dislike">
+                <span className="action-icon icon-sad-face dislike"></span>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <UserLog userAction={userAction} />
+        </section>
+        <UserLog userAction={userAction} />
+      </div>
     </section>
   );
 };
